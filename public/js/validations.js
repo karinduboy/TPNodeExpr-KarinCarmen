97292eeb76c35ee33086ad26283fdc2b8a23d165
Karin Duboy <karinduboy@gmail.com>
2019-09-27 19:23:44 -0300

    avance de validacion

--- public/js/validations.js
+++ public/js/validations.js
@@ -1,2 +1,23 @@
 //aquí colocaré las validaciones del front
-const checkName = (fieldInput) => (!fieldInput.value == '' && fieldInput.lenght <= 30 && fieldInput.lenght >= 2) ? fieldInput.className = 'valid' : fieldInput.className = 'error';
+const checkInputLenght = (fieldInput, minL, maxL) => (!fieldInput.value == '' && fieldInput.lenght <= maxL && fieldInput.lenght >= minL) ? fieldInput.className = 'valid' : fieldInput.className = 'error';
+
+const checkEmail = (fieldEmail) => {
+    let email = fieldEmail.value;
+    let exp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    let isValid = (exp.test() ? fieldEmail.className='valid':fieldEmail.className='error');   
+    return isValid; 
+};
+
+const checkPhone = (fieldInput) => (isNAN(fieldInput.value)?fieldInput.className='valid':fieldInput.className='error');
+
+const checkForm = () => {
+    inName = document.getElementById('name');
+    inEmail = document.getElementById('email');
+    inAdress = document.getElementById('address');
+    inPhone = document.getElementById('phone');
+    if (checkInputLenght(inName, 2, 30) && checkEmail(inEmail) && checkInputLenght(Address, 2, 30) && checkPhone(inPhone)) {
+        createUser();
+    } else {
+        //aqui tengo que manejar el error del formulario
+    }
+}
