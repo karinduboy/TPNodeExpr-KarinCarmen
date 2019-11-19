3d870284030a8ef7cc1d16a7200ae8370e2e8e4b
marcreativo <marcreativo@gmail.com>
2019-11-19 01:00:00 -0300

    review

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,10 @@
 //aqui colocaremos el comportamiento dinámico de la web
 
-let usersData;
+let usersData
 
 const getUsers = () => {
-    return fetch('/api/users').then((res) => res.json());
-};
+	return fetch('/api/users').then((res) => res.json())
+}
 
 const userView = ({ id, name, email, address, phone }) => `
     <tr id="${id}" class="employee">
@@ -17,17 +17,18 @@ const userView = ({ id, name, email, address, phone }) => `
         <a href="#" id="${id}" onclick="openModalDelete(this.id)"><i class="material-icons">&#xE872;</i></a>
         </td>
     </tr>
-`;
+`
 
 const printUsers = (data) => {
-    const container = document.getElementById('usersTable');
-	container.innerHTML = '';
-	data.forEach((e) => (container.innerHTML += userView(e)));
-};
+	const container = document.getElementById('usersTable')
+	container.innerHTML = ''
+	data.forEach((e) => (container.innerHTML += userView(e)))
+}
 
 const setModalContent = (modalFunction, userId) => {
-    let modalBtns = ''
-    let modalContent = `
+	// estos contenidos podrian estar mejor divididos / componetizados
+	let modalBtns = ''
+	let modalContent = `
     <div id= "modalContainer" class="modal">
                 <div class="title">
                     <h2 id="title">Agregar Usuario</h2>
@@ -55,9 +56,9 @@ const setModalContent = (modalFunction, userId) => {
                             <input type="number" id="phone" placeholder="Teléfono..." required onblur="checkPhone(this)" onkeyup="checkPhone(this)">
                             <div id="phoneError">Solo puedes introducir números</div>
                         </div>
-                    `;
-    if (modalFunction == 'Create'){
-        modalBtns = `
+                    `
+	if (modalFunction == 'Create') {
+		modalBtns = `
                         <div class="contentBtn">
                             <input type="submit" id="btn-add" value="Agregar"class="btnModal" onclick="checkForm('New')">
                             <input type="button" id="btn-cancel" value="Cancelar"class="btnModal" onclick="closeModal()">
@@ -65,9 +66,9 @@ const setModalContent = (modalFunction, userId) => {
                     </form>
                 </div>
             </div>
-        `;
-    } else if(modalFunction == 'Edit'){
-        modalBtns = `
+        `
+	} else if (modalFunction == 'Edit') {
+		modalBtns = `
                         <div class="contentBtn">
                             <input type="submit" id="${userId}" value="Agregar"class="btnModal" onclick="checkForm('Edit',this.id)">
                             <input type="button" id="btn-cancel" value="Cancelar"class="btnModal" onclick="closeModal()">
@@ -75,25 +76,25 @@ const setModalContent = (modalFunction, userId) => {
                     </form>
                 </div>
             </div>
-        `;
-    }
-    modalContent += modalBtns;
-    return modalContent;
+        `
+	}
+	modalContent += modalBtns
+	return modalContent
 }
 
 const createUser = () => {
-    event.preventDefault();
-	const formName = document.getElementById('name');
-    const formEmail = document.getElementById('email');
-    const formAddress = document.getElementById('address');
-    const formPhone = document.getElementById('phone');
-    
-    const payload  = {
-        name: formName.value,
-        email: formEmail.value,
-        address: formAddress.value,
-        phone: formPhone.value
-	};
+	event.preventDefault()
+	const formName = document.getElementById('name')
+	const formEmail = document.getElementById('email')
+	const formAddress = document.getElementById('address')
+	const formPhone = document.getElementById('phone')
+
+	const payload = {
+		name: formName.value,
+		email: formEmail.value,
+		address: formAddress.value,
+		phone: formPhone.value
+	}
 	if (checkPayload(payload, 'New')) {
 		fetch('/api/users', {
 			method: 'POST',
@@ -104,145 +105,142 @@ const createUser = () => {
 		})
 			.then((res) => res.json())
 			.then((res) => {
-                formName.value = '';
-                formEmail.value = '';
-                formAddress.value = '';
-                formPhone.value = '';
+				formName.value = ''
+				formEmail.value = ''
+				formAddress.value = ''
+				formPhone.value = ''
 			})
 			.catch((error) => {
-                //ojos los status
-			});
+				//ojos los status
+			})
 	} else {
-        let errorMsg = alert(`Todos los campos son obligatorios, por favor complétalos`)
-    }
-    closeModal();
-    location.reload(); //éste método hace que la página se actualice luego de enviar la info y se vea automáticamente el nuevo usuario, sino hay que refrescar la página manualmente.
-};
-
-const checkPayload = (payload, userType) => {
-    if (userType=='New'){
-        let isValid = (!payload.name=='' && !payload.email=='' && !payload.address=='' && !payload.phone=='')
-        ? true
-        : false;
-        return isValid;
-    } else if(userType=='Edit'){
-        let isValid = (!payload.id=='' && !payload.name=='' && !payload.email=='' && !payload.address=='' && !payload.phone=='')
-        ? true
-        : false;
-        return isValid;
-    }
-};
-
-const searchByQuery= () =>{
-    if ( event.keyCode===13){
-        let query = document.getElementById("search").value;
-        fetch(`/api/users/search/${query}`)
-        .then((res) => res.json())
-        .then(data=> printUsers(data));
-    
-    };
-        
+		let errorMsg = alert(`Todos los campos son obligatorios, por favor complétalos`)
+	}
+	closeModal()
+	location.reload() //éste método hace que la página se actualice luego de enviar la info y se vea automáticamente el nuevo usuario, sino hay que refrescar la página manualmente.
 }
 
-const clean = () =>{
-    const tbody = document.getElementById('usersTable')
-    tbody.innerHTML = ''
+const checkPayload = (payload, userType) => {
+	if (userType == 'New') {
+		let isValid =
+			!payload.name == '' && !payload.email == '' && !payload.address == '' && !payload.phone == '' ? true : false
+		return isValid
+	} else if (userType == 'Edit') {
+		let isValid =
+			!payload.id == '' &&
+			!payload.name == '' &&
+			!payload.email == '' &&
+			!payload.address == '' &&
+			!payload.phone == ''
+				? true
+				: false
+		return isValid
+	}
+}
+
+const searchByQuery = () => {
+	if (event.keyCode === 13) {
+		let query = document.getElementById('search').value
+		fetch(`/api/users/search/${query}`).then((res) => res.json()).then((data) => printUsers(data))
+	}
+}
+
+const clean = () => {
+	const tbody = document.getElementById('usersTable')
+	tbody.innerHTML = ''
 }
 
 // datos del modal
 const modalChange = (id) => {
-    let userId = id;
-    fetch(`api/users/${id}`)
-        .then((res) => res.json())
-        .then((res) => userFound = res)
-            .then( userFound =>{
-                openModal('Edit',userId)
-                const modal = document.getElementById('modalContainer');
-                const title = document.getElementById('title');
-                title.innerText = 'Editar Usuario';
-                const formName = document.getElementById('name')
-                const formEmail = document.getElementById('email')
-                const formAddress = document.getElementById('address')
-                const formPhone = document.getElementById('phone')
-                formName.value = userFound.name;
-                formEmail.value = userFound.email;
-                formAddress.value= userFound.address;
-                formPhone.value= userFound.phone;
-            })
-};
+	let userId = id
+	fetch(`api/users/${id}`).then((res) => res.json()).then((res) => (userFound = res)).then((userFound) => {
+		openModal('Edit', userId)
+		const modal = document.getElementById('modalContainer')
+		const title = document.getElementById('title')
+		title.innerText = 'Editar Usuario'
+		const formName = document.getElementById('name')
+		const formEmail = document.getElementById('email')
+		const formAddress = document.getElementById('address')
+		const formPhone = document.getElementById('phone')
+		formName.value = userFound.name
+		formEmail.value = userFound.email
+		formAddress.value = userFound.address
+		formPhone.value = userFound.phone
+	})
+}
 
 const editUser = (userId) => {
-    event.preventDefault();
-    const formName = document.getElementById('name');
-    const formEmail = document.getElementById('email');
-    const formAddress = document.getElementById('address');
-    const formPhone = document.getElementById('phone');
-    const payload  = {
-        id: userId,
-        name: formName.value,
-        email: formEmail.value,
-        address: formAddress.value,
-        phone: formPhone.value
-    }
-    console.log(payload);
-    if (checkPayload(payload, 'Edit')){
-        fetch(`api/users`, {
-            method: 'PATCH',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(payload)
-        })
-    }
-    closeModal();
-    location.reload();
-}
-    
-const openModalDelete = (id)=>{
-    const mask = document.getElementById('deleteFather');
-    mask.style.display='block';
-    const modal = document.getElementById('modalDelete');
-    modal.style.display='block';
-    const btnDelete = document.getElementById('btn-delete');
-    btnDelete.onclick = () => deleteUser(id);
-   }
-
-const deleteUser=(id)=>{ 
-    fetch(`/api/users/${id}`, {
-        method: 'DELETE',
-        headers: {'Content-Type': 'application/json'},
-    })
-        .then(res=>res.json())
-        .then(res=>{
-            console.log(res);
-        })
-     closeDelete();   
+	event.preventDefault()
+	const formName = document.getElementById('name')
+	const formEmail = document.getElementById('email')
+	const formAddress = document.getElementById('address')
+	const formPhone = document.getElementById('phone')
+	const payload = {
+		id: userId,
+		name: formName.value,
+		email: formEmail.value,
+		address: formAddress.value,
+		phone: formPhone.value
+	}
+	console.log(payload)
+	if (checkPayload(payload, 'Edit')) {
+		fetch(`api/users`, {
+			method: 'PATCH',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(payload)
+		})
+	}
+	closeModal()
+	location.reload()
 }
 
-const closeDelete=()=>{
-    const container = document.getElementById('container');
-    container.style.display='block';
-    const modal =  document.getElementById('modalDelete');
-    modal.style.display="none" 
-    location.reload();
+const openModalDelete = (id) => {
+	const mask = document.getElementById('deleteFather')
+	mask.style.display = 'block'
+	const modal = document.getElementById('modalDelete')
+	modal.style.display = 'block'
+	const btnDelete = document.getElementById('btn-delete')
+	btnDelete.onclick = () => deleteUser(id)
 }
 
+const deleteUser = (id) => {
+	fetch(`/api/users/${id}`, {
+		method: 'DELETE',
+		headers: { 'Content-Type': 'application/json' }
+	})
+		.then((res) => res.json())
+		.then((res) => {
+			console.log(res)
+		})
+	closeDelete()
+}
 
-const openModal = (modalFunction, userId)=>{
-    let modalContent = setModalContent(modalFunction, userId);
-    const modal =  document.getElementById('modalWrapper');
-    modal.innerHTML = '';
-    modal.innerHTML = modalContent;
-    modal.style.display='block';
-};
+const closeDelete = () => {
+	const container = document.getElementById('container')
+	container.style.display = 'block'
+	const modal = document.getElementById('modalDelete')
+	modal.style.display = 'none'
+	location.reload()
+}
 
-const closeModal = ()=>{
-    const modal =  document.getElementById("modalWrapper");
-    modal.style.display="none";
-};
+const openModal = (modalFunction, userId) => {
+	let modalContent = setModalContent(modalFunction, userId)
+	const modal = document.getElementById('modalWrapper')
+	modal.innerHTML = ''
+	modal.innerHTML = modalContent
+	modal.style.display = 'block'
+}
 
+const closeModal = () => {
+	const modal = document.getElementById('modalWrapper')
+	modal.style.display = 'none'
+}
 
 const initialize = async () => {
-    usersData = await getUsers();
-    printUsers(usersData.users);
-};  
+	usersData = await getUsers()
+	printUsers(usersData.users)
+}
+
+// todo muy bello.
